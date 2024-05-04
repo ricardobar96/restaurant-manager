735455540e0b4fbff2df371e1b68a1e8340e4b36
ricardobar96 <ricardobar96@gmail.com>
2024-05-04 13:24:06 +0100

    Created and positioned top panel

--- main.py
+++ main.py
@@ -10,4 +10,14 @@ app.title("Restaurant Manager - Ricardo Baloira Armas")
 
 app.config(background='seashell3')
 
+panel_top = Frame(app, bd=1, relief=FLAT)
+panel_top.pack(side=TOP)
+
+tag_title = Label(panel_top, text='Restaurant Manager', fg='chocolate2',
+                  font=('Arial', 50), bg='seashell3', width=20)
+
+tag_title.place(x=.5, y=.5, anchor = "center")
+
+tag_title.grid(row=0, column=0)
+
 app.mainloop()
